2dd84b80c05269239cb2804bf6c19504a5892bf2
gowrishvarma <106875430+gowrishvarma2003@users.noreply.github.com>
2024-08-02 20:39:05 +0530

    Update page.js
    third

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
     setError(null);
     try {
       const parsedData = JSON.parse(postData);
-      const response = await fetch('bajaj-server-production.up.railway.app:2500/api/data', {
+      const response = await fetch('bajaj-server-production.up.railway.app/api/data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -39,7 +39,7 @@ export default function Home() {
   const handleGetRequest = async () => {
     setError(null);
     try {
-      const response = await fetch('bajaj-server-production.up.railway.app:2500/api/operation_code');
+      const response = await fetch('bajaj-server-production.up.railway.app/api/operation_code');
       if (!response.ok) {
         const errorData = await response.json();
         setError(errorData.message);
